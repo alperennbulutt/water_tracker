dd663ecc34fb8a226ad82708063bf7b19448e2bf
alperennbulutt <alperenbulut.ab@gmail.com>
2022-04-12 00:13:14 +0300

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:get/get_navigation/src/routes/transitions_type.dart';
@@ -6,7 +7,10 @@ import 'package:water_tracker/src/translations/app_translations.dart';
 
 import 'src/ui/theme/app_theme.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(GetMaterialApp(
     debugShowCheckedModeBanner: false,
     initialRoute: Routes.INITIAL,
