9af6cccf4d9e78a34eac30d8d8fbfd0ae3d97d65
cbeyzaduzen <beyzaaduzen@gmail.com>
2022-05-19 21:35:26 +0300

    navigation bar sayfa tasarımları tamamlandı

--- lib/src/ui/profile/profile_page.dart
+++ lib/src/ui/profile/profile_page.dart
@@ -1,13 +1,67 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
+import 'package:intl/intl.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(
-          'profile page comes here ooor here comes the rain again my favourite page!'),
+    final now = new DateTime.now();
+    String formatter = DateFormat('yMd').format(now);
+    return Scaffold(
+      appBar: AppBar(
+        leading: GestureDetector(
+            onTap: () => {
+                  FirebaseAuth.instance.signOut(),
+                },
+            child: Icon(Icons.logout)),
+        title: Text(formatter),
+        toolbarHeight: 90,
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        flexibleSpace: ClipPath(
+          clipper: WaveClipperTwo(),
+          child: Container(
+            height: 250,
+            width: MediaQuery.of(context).size.width,
+            color: Colors.blue,
+          ),
+        ),
+      ),
+      body: Column(children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Kişisel Bilgiler",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 30,
+        ),
+        Text(
+          "Cemile Beyza",
+          style: TextStyle(fontWeight: FontWeight.w300, fontSize: 15),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Text(
+          "Düzen",
+          style: TextStyle(fontWeight: FontWeight.w300, fontSize: 15),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Text(
+          "46 kg",
+          style: TextStyle(fontWeight: FontWeight.w300, fontSize: 15),
+        )
+      ]),
     );
   }
 }
--- lib/src/ui/settings/settings_page.dart
+++ lib/src/ui/settings/settings_page.dart
@@ -1,12 +1,43 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
+import 'package:intl/intl.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Settings page!!'),
+    final now = new DateTime.now();
+    String formatter = DateFormat('yMd').format(now);
+    return Scaffold(
+      appBar: AppBar(
+        leading: GestureDetector(
+            onTap: () => {
+                  FirebaseAuth.instance.signOut(),
+                },
+            child: Icon(Icons.logout)),
+        title: Text(formatter),
+        toolbarHeight: 90,
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        flexibleSpace: ClipPath(
+          clipper: WaveClipperTwo(),
+          child: Container(
+            height: 250,
+            width: MediaQuery.of(context).size.width,
+            color: Colors.blue,
+          ),
+        ),
+      ),
+      body: Column(children: [
+        Row(
+          children: [
+            Text("Bildirim aç/kapat : "),
+            Switch(value: true, onChanged: (value) {}),
+          ],
+        ),
+      ]),
     );
   }
 }
